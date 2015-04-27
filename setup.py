ca9e71dbb101bb6ac514ebf99d3196d3955a19ed
amureki <fly.amureki@gmail.com>
2015-04-27 23:19:48 +0500

    added to pypi

---
+++ setup.py
@@ -0,0 +1,14 @@
+import sys
+from setuptools import setup, find_packages
+
+setup(
+    name='pleercom-py',
+    version='0.1',
+    description='Pleer.com API Wrapper',
+    author='Rustem Sayargaliev',
+    author_email='r.sayargaliev@gmail.com',
+    license='MIT',
+    url='https://github.com/amureki/pleercom-py.git',
+    packages=find_packages(),
+    zip_safe=False
+)
